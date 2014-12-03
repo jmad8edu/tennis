fc521293818f3a8776421e70c25acf4032aeff5f
Justin Madsen <justinmadsen4@gmail.com>
2014-12-03 15:45:33 -0700

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   resources :users
-
+  
+  get 	 'about'   => 'static_pages#about'
+  get 	 'contact' => 'static_pages#contact'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
